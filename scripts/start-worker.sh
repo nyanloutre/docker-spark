1ff8fa3c51f1f710017231f8ab433b21b964d262
Paul TREHIOU <nyanloutre@users.noreply.github.com>
2016-09-09 09:42:27 +0200

    Update start-worker.sh

--- scripts/start-worker.sh
+++ scripts/start-worker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 /usr/local/bin/spark-class org.apache.spark.deploy.worker.Worker \
-	spark://${SPARK_MASTER_PORT_7077_TCP_ADDR}:${SPARK_MASTER_ENV_SPARK_MASTER_PORT} \
+	spark://localhost:7077 \
 	--properties-file /spark-defaults.conf
 /bin/bash
