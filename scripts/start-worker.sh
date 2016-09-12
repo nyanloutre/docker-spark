6404fb3072687328d4c8b01362876e4bdb93f6b9
Paul TREHIOU <nyanloutre@users.noreply.github.com>
2016-09-12 10:14:14 +0200

    Update start-worker.sh

--- scripts/start-worker.sh
+++ scripts/start-worker.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 /usr/local/bin/spark-class org.apache.spark.deploy.worker.Worker \
 	spark://spark-master:7077 \
-	--properties-file /spark-defaults.conf \
-	--host spark-worker
+	--properties-file /spark-defaults.conf
 tail -f /dev/null
